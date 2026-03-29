e7bc22c52d00042b53708b2a1b7625637151792d
Hasancan <hasofreeman@gmail.com>
2026-03-29 15:02:49 +0300

    Update matrix_lib.py

--- matrix_lib.py
+++ matrix_lib.py
@@ -1,3 +1,15 @@
+"""
+LIBRARY: Matrix Operations Library
+PURPOSE: Custom matrix library for structural analysis. Handles symmetric banded storage.
+NOTE: No external libraries like NumPy are used.
+"""
+
+class Matrix:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.data = [[0.0 for _ in range(cols)] for _ in range(rows)]
+
 class BandedSymmetricMatrix:
     def __init__(self, size, half_bandwidth):
         self.n = size
@@ -9,13 +21,6 @@ class BandedSymmetricMatrix:
         offset = j - i
         if 0 <= offset <= self.bw:
             self.data[i][offset] += value
-
-    def set_element(self, i, j, value):
-        """Reset and set a specific element to a new value."""
-        if i > j: i, j = j, i
-        offset = j - i
-        if 0 <= offset <= self.bw:
-            self.data[i][offset] = value
 
     def get_element(self, i, j):
         if i > j: i, j = j, i
